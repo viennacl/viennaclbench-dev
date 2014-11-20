b0a6583eb8573332451fedb5758df544ff56f5de
Karl Rupp <rupp@iue.tuwien.ac.at>
2014-11-20 18:27:28 +0100

    BLAS3: Now plotting results for different matrix sizes.

--- src/benchmarks/benchmark_blas3.cpp
+++ src/benchmarks/benchmark_blas3.cpp
@@ -63,130 +63,51 @@ template<typename ScalarType>
 void Benchmark_Blas3::run_benchmark()
 {
   Timer timer;
-  double exec_time;
   int testId = 0;
 
-//  std::cout << "Benchmarking..." << std::endl;
-//  std::cout << "Platform id: "<< viennacl::ocl::current_context().platform_index() //platform id != context id
-//            <<" Context value: " << viennacl::ocl::current_context().handle().get() << std::endl;
+  for (std::size_t N=128; N<=blas3MatrixSizeA; N *= 2)
+  {
+    viennacl::matrix<ScalarType> vcl_A(N, N);
+    viennacl::matrix<ScalarType> vcl_B(N, N);
+    viennacl::matrix<ScalarType> vcl_C(N, N);
 
-//  std::cout << "Running on device name: "<< viennacl::ocl::current_device().name() << std::endl;
-  //
-  // Set up some ViennaCL objects
-  //
+    //
+    // One alternative: Put the matrices into a contiguous block of memory (allows to use viennacl::fast_copy(), avoiding temporary memory)
+    //
+    std::vector<ScalarType> stl_A(vcl_A.internal_size());
+    std::vector<ScalarType> stl_B(vcl_B.internal_size());
 
-  //viennacl::ocl::current_context().build_options("-cl-mad-enable -cl-fast-relaxed-math");   //uncomment for additional optimizations
-  //viennacl::ocl::current_context().build_options("-cl-opt-disable");                        //uncomment to get poor performance
-  viennacl::matrix<ScalarType> vcl_A(blas3MatrixSizeA, blas3MatrixSizeB);
-  viennacl::matrix<ScalarType> vcl_B(blas3MatrixSizeB, blas3MatrixSizeC);
-  viennacl::matrix<ScalarType> vcl_C(blas3MatrixSizeA, blas3MatrixSizeC);
+    //
+    // Fill the matrix
+    //
+    for (unsigned int i = 0; i < N; ++i)
+      for (unsigned int j = 0; j < N; ++j)
+        stl_A[i*N + j] = random<ScalarType>();
 
-  //
-  // One alternative: Put the matrices into a contiguous block of memory (allows to use viennacl::fast_copy(), avoiding temporary memory)
-  //
-  std::vector<ScalarType> stl_A(vcl_A.internal_size());
-  std::vector<ScalarType> stl_B(vcl_B.internal_size());
+    for (unsigned int i = 0; i < N; ++i)
+      for (unsigned int j = 0; j < N; ++j)
+        stl_B[i + j*N] = random<ScalarType>();
 
-  //
-  // Fill the matrix
-  //
-  for (unsigned int i = 0; i < blas3MatrixSizeA; ++i)
-    for (unsigned int j = 0; j < blas3MatrixSizeB; ++j)
-      stl_A[i*blas3MatrixSizeA + j] = random<ScalarType>();
+    //  std::cout << " ------ Benchmark 1: Matrix-Matrix product ------ " << std::endl;
 
-  for (unsigned int i = 0; i < blas3MatrixSizeB; ++i)
-    for (unsigned int j = 0; j < blas3MatrixSizeC; ++j)
-      stl_B[i + j*blas3MatrixSizeC] = random<ScalarType>();
+    viennacl::fast_copy(&(stl_A[0]), &(stl_A[0]) + stl_A.size(), vcl_A);
+    viennacl::fast_copy(&(stl_B[0]), &(stl_B[0]) + stl_B.size(), vcl_B);
+    vcl_C = viennacl::linalg::prod(vcl_A, vcl_B); // warmup
+    viennacl::backend::finish();
+    timer.start();
+    vcl_C = viennacl::linalg::prod(vcl_A, vcl_B);
+    viennacl::backend::finish();
+    double exec_time = timer.get();
 
+    double gflops_per_second = 2.0 * (vcl_A.size1() / 1000.0) * (vcl_A.size2() / 1000.0) * (vcl_B.size2() / 1000.0) / exec_time ;
+    emit resultSignal("Matrix-Matrix product", N, gflops_per_second, LINE_GRAPH, testId);
+    testResultHolder.append(gflops_per_second);
+    emit testProgress();
 
-  double tempResultValue;
+    if (exec_time > 10.0) // we don't want the benchmark to get stuck here. The relevant information can already be obtained with less than 10 seconds of execution time.
+      break;
+  }
 
-  /////////////////////////////////////////////////
-  //////////// Matrix-matrix products /////////////
-  /////////////////////////////////////////////////
-
-  //  std::cout << " ------ Benchmark 1: Matrix-Matrix product ------ " << std::endl;
-
-  viennacl::fast_copy(&(stl_A[0]),
-      &(stl_A[0]) + stl_A.size(),
-      vcl_A);
-  viennacl::fast_copy(&(stl_B[0]),
-      &(stl_B[0]) + stl_B.size(),
-      vcl_B);
-  vcl_C = viennacl::linalg::prod(vcl_A, vcl_B);
-  viennacl::backend::finish();
-  timer.start();
-  vcl_C = viennacl::linalg::prod(vcl_A, vcl_B);
-  viennacl::backend::finish();
-  exec_time = timer.get();
-
-  tempResultValue = 2.0 * (vcl_A.size1() / 1000.0) * (vcl_A.size2() / 1000.0) * (vcl_B.size2() / 1000.0) / exec_time ;
-  emit resultSignal("Matrix-Matrix product", testResultHolder.size(), tempResultValue, BAR_GRAPH, testId );
-  testResultHolder.append(tempResultValue);
-  emit testProgress();
-
-  //  std::cout << " ------ Benchmark 2: Matrix-Matrix product using ranges ------ " << std::endl;
-
-  viennacl::range r(blas3MatrixSizeB/4, 3 * blas3MatrixSizeB/4);
-
-  viennacl::fast_copy(&(stl_A[0]),
-      &(stl_A[0]) + stl_A.size(),
-      vcl_A);
-  viennacl::fast_copy(&(stl_B[0]),
-      &(stl_B[0]) + stl_B.size(),
-      vcl_B);
-  viennacl::project(vcl_C, r, r) = viennacl::linalg::prod(viennacl::project(vcl_A, r, r), viennacl::project(vcl_B, r, r));
-  viennacl::backend::finish();
-  timer.start();
-  viennacl::project(vcl_C, r, r) = viennacl::linalg::prod(viennacl::project(vcl_A, r, r), viennacl::project(vcl_B, r, r));
-  viennacl::backend::finish();
-  exec_time = timer.get();
-
-  tempResultValue = 2.0 * (vcl_A.size1() / 2000.0) * (vcl_A.size2() / 2000.0) * (vcl_B.size2() / 2000.0) / exec_time ;
-  emit resultSignal("Matrix-Matrix product using ranges", testResultHolder.size(), tempResultValue, BAR_GRAPH, testId );
-  testResultHolder.append(tempResultValue);
-  emit testProgress();
-
-  //  std::cout << " ------ Benchmark 3: Matrix-Matrix product using slices ------ " << std::endl;
-
-  viennacl::slice s(0, 2, blas3MatrixSizeB/2);
-
-  viennacl::fast_copy(&(stl_A[0]),
-      &(stl_A[0]) + stl_A.size(),
-      vcl_A);
-  viennacl::fast_copy(&(stl_B[0]),
-      &(stl_B[0]) + stl_B.size(),
-      vcl_B);
-  viennacl::project(vcl_C, s, s) = viennacl::linalg::prod(viennacl::project(vcl_A, s, s), viennacl::project(vcl_B, s, s));
-  viennacl::backend::finish();
-  timer.start();
-  viennacl::project(vcl_C, s, s) = viennacl::linalg::prod(viennacl::project(vcl_A, s, s), viennacl::project(vcl_B, s, s));
-  viennacl::backend::finish();
-  exec_time = timer.get();
-
-  tempResultValue = 2.0 * (vcl_A.size1() / 2000.0) * (vcl_A.size2() / 2000.0) * (vcl_B.size2() / 2000.0) / exec_time;
-  emit resultSignal("Matrix-Matrix product using slices", testResultHolder.size(), tempResultValue, BAR_GRAPH, testId );
-  testResultHolder.append(tempResultValue);
-  emit testProgress();
-/*
- * LU factorization is disabled (does not support non-square matrices)
-  //  std::cout << " ------ Benchmark 4: LU factorization ------ " << std::endl;
-
-  viennacl::fast_copy(&(stl_A[0]),
-      &(stl_A[0]) + stl_A.size(),
-      vcl_A);
-  viennacl::linalg::lu_factorize(vcl_A);
-  viennacl::backend::finish();
-  timer.start();
-  viennacl::linalg::lu_factorize(vcl_A);
-  viennacl::backend::finish();
-  exec_time = timer.get();
-
-  tempResultValue = 2.0 * (vcl_A.size1() / 1000.0) * (vcl_A.size2() / 1000.0) * (vcl_A.size2() / 1000.0) / exec_time;
-  emit resultSignal("LU factorization", testResultHolder.size(), tempResultValue, BAR_GRAPH, testId );
-  testResultHolder.append(tempResultValue);
-  emit testProgress();
-*/
 }
 
 /*!
@@ -195,13 +116,12 @@ void Benchmark_Blas3::run_benchmark()
 void Benchmark_Blas3::execute()
 {
   emit benchmarkStarted(BLAS3);
-  emit unitMeasureSignal("GFLOPs", Qt::XAxis);
+  emit unitMeasureSignal("GFLOPs", Qt::YAxis);
+  emit unitMeasureSignal("Matrix Size", Qt::XAxis);
 
   //Single Precision
   if(getPrecision() == SINGLE_PRECISION)
-  {
     run_benchmark<float>();
-  }
 
   //Double Precision
   else if( getPrecision() == DOUBLE_PRECISION)
@@ -216,6 +136,6 @@ void Benchmark_Blas3::execute()
   }
 
   qSort(testResultHolder);//sort test results in ascending order
-  emit finalResultSignal("Blas3", testResultHolder[testResultHolder.size()/2]);
+  emit finalResultSignal("Blas3", testResultHolder[testResultHolder.size()-1]);
   emit benchmarkComplete();
 }
