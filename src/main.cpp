df76e52a92b50e9c4ca332fb740d297a2acd4cef
Karl Rupp <rupp@iue.tuwien.ac.at>
2014-11-27 15:52:44 +0100

    Splash screen: Additional flag to show it in foreground.
    On my Linux Mint Maya system the splash screen was hiding behind the terminal.

--- src/main.cpp
+++ src/main.cpp
@@ -75,8 +75,9 @@ int main(int argc, char *argv[])
   QApplication app(argc, argv);
   SplashScreen *splashScreen = new SplashScreen();
 
-  //I really hate attention-seeking apps. There is no need to steal focus from other applications. Unfortunately, this does not seem to work on Windows...
+  //Make sure splash screen is not hidden by other applications:
   splashScreen->setAttribute(Qt::WA_ShowWithoutActivating);
+  splashScreen->setWindowFlags(Qt::WindowStaysOnTopHint | Qt::SplashScreen);
 
   splashScreen->setPixmap(QPixmap(":/resources/images/splash-screen.jpg"));
   splashScreen->show();
